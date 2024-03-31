ad1f854928ed12a331257b2a6f35898b3c2cc6e8
acrowfliedover <leofanrong@gmail.com>
2024-03-31 00:15:50 -0700

    assignment9preview

---
+++ CS605/9Module/libAssignment9.s
@@ -0,0 +1,224 @@
+#
+# Library name: libAssignment9.s
+# Author: Rong Fan
+# Date: 03/30/2024
+# Contents:
+# 1.1: checkAlphaChar			go to line 11
+# 1.2: checkAlphaCharWithoutLogical	go to line 58
+# 2: gradeCheck				go to line 110
+# 3: findMaxOf3				go to line 189
+
+.global checkAlphaChar
+
+.text
+
+checkAlphaChar:
+	# input: a word representing a char in r0 
+	# output: a boolean in r0
+	# 0 if input is not an alpha character, 1 if it is
+	# the ascii value of alpha chars: 0x40 < c <= 0x5A or 0x60 < c <= 0x7A
+
+# store lr on stack
+	SUB sp, sp, #4
+	STR lr, [sp]
+	
+# use logical variables in r1-r3 	
+	MOV r1, #0
+	MOV r2, #0
+	MOV r3, #0
+	
+# first part of requirement in r1
+	CMP r0, #0x40
+	MOVGT r2, #1
+	CMP r0, #0x5A
+	MOVLE r3, #1
+	AND r1, r2, r3
+
+# second part of requirement in r2
+	MOV r2, #0
+	MOV r3, #0
+	CMP r0, #0x60
+	MOVGT r2, #1
+	CMP r0, #0x7A
+	MOVLE r3, #1
+	AND r2, r2, r3
+
+# output result
+	ORR r0, r1, r2
+	
+# move lr back from stack and return
+	LDR lr, [sp]
+	ADD sp, #4
+	MOV pc, lr
+.data
+
+# End checkAlphaChar
+
+
+.global checkAlphaCharWithoutLogical
+
+.text
+checkAlphaCharWithoutLogical:
+	# input: a word representing a char in r0
+	# output: a boolean in r0
+	# 0 if input is not an alpha character, 1 if it is
+	# 0x40 < c <= 0x5A or 0x60 < c <= 0x7A
+	# This time does not use logical variables
+	# This honestly looks cleaner than I expected
+	# Also beware that branching takes much longer than other instructions
+	# Not sure if this is spaghette code, as it reuses branches
+	
+# store lr on stack
+	SUB sp, sp, #4
+	STR lr, [sp]
+
+# first check 
+	CMP r0, #0x40
+	BLE output0
+
+# second check
+	CMP r0, #0x5A
+	BLE output1
+
+# third check
+	CMP r0, #0x60	
+	BLE output0
+
+# fourth check
+	CMP r0, #0x7A
+	BLE output1
+	
+# else
+output0:
+	MOV r0, #0
+	B output
+
+output1:
+	MOV r0, #1	
+	
+output:
+# move lr back from stack and return
+	LDR lr, [sp]
+	ADD sp, #4
+	MOV pc, lr
+
+.data
+
+# End checkAlphaCharWithoutLogical
+
+
+.global gradeCheck
+ 
+.text
+gradeCheck:
+	# input: integer representing a grade in r0
+	# output: to console the grade letter
+	# g < 0 OR g > 100 output 'E' for error (technically we can go above 100 from extra credits)
+	# g >= 90 output A
+	# else g >= 80 output B
+	# else g >= 70 output C
+	# else output F
+	
+# store lr on stack
+	SUB sp, sp, #4
+	STR lr, [sp]
+
+# move r0 to a safe space
+	SUB sp, sp, #4
+	STR r5, [sp]
+	MOV r5, r0	
+
+# check for error 0
+	CMP r5, #0 
+	BGE checkF 
+		LDR r0, =outputE
+		BL printf
+		B outputGrade
+		
+# check for grade F
+checkF:
+	CMP r5, #70
+	BGE checkC
+		LDR r0, =outputF
+		BL printf
+		B outputGrade
+
+# check for grade C
+checkC:
+	CMP r5, #80
+	BGE checkB
+		LDR r0, =outputC
+		BL printf
+		B outputGrade
+
+# check for grade B
+checkB:
+	CMP r5, #90
+	BGE checkA
+		LDR r0, =outputB
+		BL printf
+		B outputGrade
+		
+# check for grade A
+checkA:
+	CMP r5, #100
+	BGT error
+		LDR r0, =outputA
+		BL printf
+		B outputGrade
+
+# else error g > 100  
+error:
+	LDR r0, =outputE
+	BL printf
+	
+# move lr back from stack and return
+outputGrade:
+	LDR r5, [sp]
+	ADD sp, #4
+	LDR lr, [sp]
+	ADD sp, #4
+	MOV pc, lr
+
+.data
+	outputE: .ascii "Your grade is outside of normal range 0 <= g <= 100. \n"
+	outputF: .ascii "Your grade is F. \n"
+	outputC: .ascii "Your grade is C. \n"
+	outputB: .ascii "Your grade is B. \n"
+	outputA: .ascii "Your grade is A. \n"
+
+# End gradeCheck
+
+
+.global findMaxOf3
+
+.text
+findMaxOf3:
+	# input: three integers in r0, r1, r2
+	# output: the largest integer in r0
+	# find the max of r0 and r1 and store in r0
+	# find the max of r0 and r2 and store in r0
+
+# store lr on stack
+	SUB sp, sp, #4
+	STR lr, [sp]
+
+# find the max of r0 and r1 and store in r0
+	CMP r0, r1
+	BGE step2
+		MOV r0, r1
+	
+# find the max of r0 and r2 and store in r0
+step2:
+	CMP r0, r2
+	BGE outputMax
+		MOV r0, r2
+	
+outputMax:
+# add back to stack
+	LDR lr, [sp]
+	ADD sp, #4
+	MOV pc, lr
+
+.data
+# End findMaxOf3
---
+++ CS605/9Module/mainAssignment9.s
@@ -0,0 +1,181 @@
+#
+# File name: mainAssignment9.s
+# Author: Rong Fan
+# Date: 03/30/2024
+# Purpose: This is the main program used to test and run the 4 functions in libAssignment9.s
+
+.text
+.global main
+
+main:
+# store lr on stack
+	SUB sp, sp, #4
+	STR lr, [sp]
+
+# Prompt user for a function to use
+	LDR r0, =askFunction
+	BL printf
+
+input:
+# Input number
+	LDR r0, =formatDecimal
+	LDR r1, =action
+	BL scanf
+
+# Load number inputted
+	LDR r0, =action
+	LDR r4, [r0]
+
+# Case 1 run function checkAlphaChar
+	CMP r4, #1
+	BNE case2
+		LDR r0, =askChar
+		BL printf
+		LDR r0, =formatChar
+		LDR r1, =character
+		BL scanf
+		LDR r0, =character
+		LDR r0, [r0]
+		BL checkAlphaChar
+	# Processing output
+		CMP r0, #0
+		BEQ notLetter
+			LDR r0, =outputIsLetter
+			BL printf
+			B endProgram	
+		notLetter:
+		LDR r0, =outputIsNotLetter
+		BL printf
+		B endProgram
+
+# Case 2 run function checkAlphaCharWithoutLogical
+case2:
+	CMP r4, #2
+	BNE case3
+		LDR r0, =askChar
+		BL printf
+		LDR r0, =formatChar
+		LDR r1, =character
+		BL scanf
+		LDR r0, =character
+		LDR r0, [r0]
+		BL checkAlphaCharWithoutLogical
+
+	# Processing output
+		CMP r0, #0
+		BEQ notLetter2
+			LDR r0, =outputIsLetter
+			BL printf
+			B endProgram
+		notLetter2:
+		LDR r0, =outputIsNotLetter
+		BL printf
+		B endProgram
+
+# Case 3 run function gradeCheck
+case3:
+	CMP r4, #3
+	BNE case4
+	# Ask user for name
+		LDR r0, =askName
+		BL printf
+		LDR r0, =formatString
+		LDR r1, =name
+		BL scanf
+
+	# Ask user for grade
+		LDR r0, =askGrade
+		BL printf
+		LDR r0, =formatDecimal
+		LDR r1, =grade
+		BL scanf
+
+	# Processing input, note that outputting grade is done in gradeCheck
+		LDR r0, =outputName
+		LDR r1, =name
+		BL printf
+		LDR r0, =grade
+		LDR r0, [r0]
+		BL gradeCheck
+		B endProgram
+
+# Case 4 run function findMaxOf3
+case4:
+	CMP r4, #4
+	BNE case0
+	# Ask user for 3 integers
+		LDR r0, =askFirstInt
+		BL printf
+		LDR r0, =formatDecimal
+		LDR r1, =int1
+		BL scanf
+
+		LDR r0, =askSecondInt
+		BL printf
+		LDR r0, =formatDecimal
+		LDR r1, =int2
+		BL scanf
+
+		LDR r0, =askLastInt
+		BL printf
+		LDR r0, =formatDecimal
+		LDR r1, =int3
+		BL scanf
+
+	# Process input
+		LDR r0, =int1
+		LDR r0, [r0]
+		LDR r1, =int2
+		LDR r1, [r1]
+		LDR r2, =int3
+		LDR r3, [r3]
+		BL findMaxOf3
+	
+	# Output
+		MOV r1, r0
+		LDR r0, =outputMax
+		BL printf
+		B endProgram
+
+# Case 0 exit the program, else return to input
+case0:
+	CMP r4, #0
+	BEQ endProgram
+		LDR r0, =askNewCommand
+		BL printf
+		B input
+
+endProgram:
+# move lr back and return
+	LDR lr, [sp]
+	ADD sp, #4
+	MOV pc, lr
+
+.data
+	askFunction: .asciz "Hello user 10001, please give a me a number to test for functions.\nEnter [1] to test checkAlphaChar. \nEnter [2] to test checkAlphaCharWithoutLogical. \nEnter [3] to test gradeCheck. \nEnter [4] to test findMaxOf3. \nEnter [0] to exit. \n"
+	askChar: .asciz "Please enter a character and I will tell you whether or not it's a letter. \n"
+	askName: .asciz "Please enter your name. \n"
+	askGrade: .asciz "Please enter your grade that is an integer. \n"
+	askFirstInt: .asciz "Please enter an integer \n"
+	askSecondInt: .asciz "Please enter another integer \n"
+	askLastInt: .asciz "Please enter the last integer \n"
+	askNewCommand: .asciz "You entered a non-valid command, please enter a correct number. \n"
+
+	formatDecimal: .asciz "%d"
+	formatChar: .asciz "%s"
+	formatString: .asciz "%s"	
+
+	outputIsLetter: .asciz "You entered a alphabetic character. \n"
+	outputIsNotLetter: .asciz "You entered a non-alphabetic character. \n"
+	outputName: .asciz "Hi %s, "
+	outputMax: .asciz "The max of the three is %d \n"
+	
+	action: .word 0
+	character: .word 0
+	name: .space 20
+	grade: .word 0
+	int1: .word 0
+	int2: .word 0
+	int3: .word 0
+
+# End main
