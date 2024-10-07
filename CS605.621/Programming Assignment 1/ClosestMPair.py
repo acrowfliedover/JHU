4f075141295a6ae80f58238ce1a1e4db5af5e2fb
acrowfliedover <leofanrong@gmail.com>
2024-10-07 00:09:25 -0700

    feat(adding new class): upgrade course title
    Adding python project CS 605.621

---
+++ CS605.621/Programming Assignment 1/ClosestMPair.py
@@ -0,0 +1,9 @@
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+def sum(a, b):
+    return (a + b)
+
+a = float(input('Enter 1st number: '))
+b = float(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
